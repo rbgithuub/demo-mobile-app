485efca86d8eae293dfd3974a7101e04fab73337
Balaji Raja <net.balajiraja@gmail.com>
2023-11-04 15:56:59 +0530

    mobile app login project with image changes

---
+++ mobile_app_image/android/app/src/main/kotlin/com/gt/mobile_app_image/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gt.mobile_app_image
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mobile_app_image/lib/mobile_app_image.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+class app_image extends StatelessWidget {
+  const app_image({super.key});
+
+  @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlue,
+          leading: Icon(Icons.menu),
+          title: Center(child: Text("Login Demo \(Image)")),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.minimize),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.maximize_rounded),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.close),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image(image: AssetImage("assets/img/flower.jpeg")),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.login_outlined),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 3, color: Colors.black54
+                    ),
+                  ),
+                  hintText: '',
+                  labelText: 'Enter User Name',
+                )
+                ),
+              ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.password_outlined),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 3, color: Colors.black54
+                    ),
+                  ),
+                  hintText: '',
+                  labelText: 'Enter Password',
+                ),
+              ),
+            )
+
+
+          ],
+        )
+      );
+  }
+  }
+
