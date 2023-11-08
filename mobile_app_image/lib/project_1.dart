7d307760eacc21b42b2cc5bf617cabe2a181299c
Balaji Raja <net.balajiraja@gmail.com>
2023-11-08 17:04:45 +0530

    New project_one GT changes

--- mobile_app_image/lib/project_1.dart
+++ mobile_app_image/lib/project_1.dart
@@ -9,9 +9,7 @@ class project_one extends StatelessWidget {
             backgroundColor: Colors.blueGrey,
             leading: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Image.asset(
-                  "assets/img/gt_logo.png",
-                )
+                child: Image.asset("assets/img/gt_logo.png")
             ),
           title: Center(child: Text("GT")),
           actions: [
@@ -25,53 +23,54 @@ class project_one extends StatelessWidget {
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 250,
-                width: 350,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.lime,
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(25)
-                ),
-                child: Column(
-                  children: [
-                    Text("WELCOME"),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.login_outlined),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              width: 3, color: Colors.black54
+              child: Center(
+                child: Container(
+                  height: 250,
+                  width: 350,
+                  decoration: BoxDecoration(
+                    color: Colors.lime,
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(25)
+                  ),
+                  child: Column(
+                    children: [
+                      Text("WELCOME"),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.login_outlined),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                width: 3, color: Colors.black54
+                              ),
                             ),
+                            hintText: '',
+                            labelText: 'Enter User Name'
+                            )
                           ),
-                          hintText: '',
-                          labelText: 'Enter User Name'
-                          )
-                        ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          prefixIcon: Icon(Icons.password_outlined),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              width: 3, color: Colors.black54
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.password_outlined),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                width: 3, color: Colors.black54
+                              ),
                             ),
+                            hintText: '',
+                            labelText: 'Enter Password'
                           ),
-                          hintText: '',
-                          labelText: 'Enter Password'
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(onPressed: (){}, style: ButtonStyle(shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)))), child: Text("Login"),),
-                    )
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(onPressed: (){}, style: ButtonStyle(shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)))), child: Text("Login"),),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -88,22 +87,40 @@ class project_one extends StatelessWidget {
                 )
                 ),
                 child: Row (
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image(image: AssetImage("assets/img/gt_logo_1.png")),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image(image: AssetImage("assets/img/gt_logo_2.png")),
-                    ),
-                  ],
+                 children: [
+                   Column(
+                     children: [
+                       Padding(
+                         padding: const EdgeInsets.all(8.0),
+                         child: Image(image: AssetImage("assets/img/gt_logo_1.png")),
+                       )
+                     ],
+                   ),
+                   Column(
+                     children: [
+                       Padding(
+                         padding: const EdgeInsets.all(8.0),
+                         child: Container(
+                             width: 100,
+                             child: Text("All the best for your Internship.",overflow: TextOverflow.ellipsis,maxLines: 3,)),
+                       )
+                     ],
 
-                )
-              ),
+                   ),
+                   Column(
+                     children: [
+                       Container(child: Padding(
+                         padding: const EdgeInsets.all(8.0),
+                         child: Image(image: AssetImage("assets/img/gt_logo_2.png")),
+                       ))
+                     ],
+                   )
+                     ],
+                   )
+    )
             )
-          ],
-        ),
+                 ],
+    )
     );
   }
 }
