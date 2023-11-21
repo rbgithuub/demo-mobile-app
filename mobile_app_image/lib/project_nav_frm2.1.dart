b89a760376e2e18d26e4f1fe99edf118103408ee
Balaji Raja <net.balajiraja@gmail.com>
2023-11-21 18:40:27 +0530

    Project to have navigation bottom bar - day-8

--- mobile_app_image/lib/project_nav_frm2.1.dart
+++ mobile_app_image/lib/project_nav_frm2.1.dart
@@ -1,16 +1,48 @@
+
 import 'package:flutter/material.dart';
+import 'project_nav_frm2.2.dart';
+
 class project_nav_frm2_1 extends StatelessWidget {
   const project_nav_frm2_1({super.key});
 
   @override
     Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Image.asset("assets/img/navigation.jpg"),
-        ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        leading: Image.asset('assets/img/ivu-tech-nxt.png'),
+        title: Center(child: Text("IVU Tech Next",style: TextStyle(color: Colors.red),)),
+        actions: [
+          Icon(Icons.menu),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: ClipRect(
+              child: Image(
+                image: AssetImage('assets/img/ivu-tech-nxt.png'),
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              ElevatedButton(onPressed: (){},style: ButtonStyle(shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)))), child: Text("About Us"),),
+              ElevatedButton(onPressed: (){},style: ButtonStyle(shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)))), child: Text("Departments"),),
+              ElevatedButton(onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const project_nav_frm2_2()),
+                );
+              },style: ButtonStyle(shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)))), child: Text("Reports"),),
+              ElevatedButton(onPressed: (){},style: ButtonStyle(shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)))), child: Text("Tools"),),
+              ElevatedButton(onPressed: (){},style: ButtonStyle(shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)))), child: Text("Projects"),),
+              ],
+          )
+        ],
+
       ),
     );
   }
-  }
+}
